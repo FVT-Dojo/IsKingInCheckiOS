e54446355ffac44e10abba20f444f699d529d2f2
Arthur van Aarssen <arthur.vanaarssen@ns.nl>
2024-05-01 11:18:06 +0200

    Initial Commit

---
+++ IsKingInCheckiOS/IsKingInCheckiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsKingInCheckiOSApp.swift
+//  IsKingInCheckiOS
+//
+//  Created by Arthur van Aarssen on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct IsKingInCheckiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
